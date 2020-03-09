de5af3352b79f2a972aae345a1a7e29b63e05f4c
Andrea Brenes <andrea@Andreas-MacBook-Pro.local>
2020-03-09 19:53:44 +0100

    First Commit

---
+++ code/canvas.tsx
@@ -0,0 +1,4 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const ComponentFromCode = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_RtIje3quE", {}, 256,148);
